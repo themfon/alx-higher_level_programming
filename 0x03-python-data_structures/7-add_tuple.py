daa906f7c1a9175f21ae555c9cdb8704fd2b0715
Mfon <92741946+themfon@users.noreply.github.com>
2022-06-06 11:44:00 +0100

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,12 +17,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1 = 0
         b2 = 0
     elif len_b == 1:
-        b1 = tuple_b[0] 
+        b1 = tuple_b[0]
         b2 = 0
     else:
         b1 = tuple_b[0]
-        b2 = tuple_b[1]               
+        b2 = tuple_b[1]
 
-        new_tuple = (a1 + b1, a2 + b2)
+    new_tuple = (a1 + b1, a2 + b2)
 
-        return (new_tuple)
+    return (new_tuple)
